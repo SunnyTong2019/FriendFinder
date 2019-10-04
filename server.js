67cc04779827d2f013ce92ae303c80a588ee9438
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2019-10-03 23:36:42 -0700

    changed order of routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
-var htmlRoutes = require('./app/routing/htmlRoutes.js');
 var apiRoutes = require('./app/routing/apiRoutes.js');
+var htmlRoutes = require('./app/routing/htmlRoutes.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,8 +9,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-htmlRoutes(app);
 apiRoutes(app);
+htmlRoutes(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
