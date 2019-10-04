56e58d066519a526d8d9b72d492cf9f71566fbf6
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2019-10-03 17:00:46 -0700

    added user input validation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,27 +6,26 @@ module.exports = function (app) {
         return res.json(friends);
     });
 
-
     app.post("/api/friends", function (req, res) {
         var newFriend = req.body;
 
         var diff = [];
 
-        for (var i=0; i<friends.length; i++){
+        for (var i = 0; i < friends.length; i++) {
             diff.push(checkDiff(friends[i].scores, newFriend.scores));
         }
-        
+
         var index = diff.indexOf(Math.min(...diff));
-       
+
         res.json(friends[index]);
 
         friends.push(newFriend);
     });
 
-    function checkDiff(arr1,arr2) {
+    function checkDiff(arr1, arr2) {
         var totalDiff = 0;
-        for (var i=0; i<10; i++) {
-            totalDiff += Math.abs(arr1[i]-arr2[i]);
+        for (var i = 0; i < 10; i++) {
+            totalDiff += Math.abs(arr1[i] - arr2[i]);
         }
         return totalDiff;
     }
